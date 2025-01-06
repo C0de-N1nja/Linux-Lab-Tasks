711dc47c44f3faf873eb14e285d887a4172bb497
C0de-N1nja <rehanbaig2003@gmail.com>
2025-01-06 20:07:57 +0500

    Warning: Run before the 'shared-memory'reader' program

--- shared-memory-writer.cpp
+++ shared-memory-writer.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
-#include<stdio.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 using namespace std;
 
-int main(){
-    key_t key = ftok("shmfile", 65);
-    int shmget(key, 1024, 0666 | IPC_CREAT);
-    char* str = (char*)shmat(shmid, void*)0,0);
-    cout<<"Write DAta: ";
-    cin.getline(str,1024);
-    cout<<"Data written in memory: "<<str<<endl;
-    shmdt(str);
+int main() {
+    key_t key = ftok("shmfile", 65); // Generate unique key
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT); // Create shared memory
+
+    char* str = (char*)shmat(shmid, (void*)0, 0); // Attach shared memory
+    cout << "Write Data: ";
+    cin.getline(str, 1024); // Get user input
+    cout << "Data written in memory: " << str << endl;
+
+    shmdt(str); // Detach shared memory
     return 0;
 }
